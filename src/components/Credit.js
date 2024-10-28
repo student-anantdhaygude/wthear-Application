d94cd093e8da075722b99a1316dbfe4e600f4d4b
student anantdhaygude <student anantdhaygude709@gmail.com>
2024-10-28 11:59:15 +0530

    fist commit

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by Anant Dhaygude
+
+      
+    </p>
+  </div>
+  );
+}
